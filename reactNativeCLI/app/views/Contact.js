cf0e77970edf790561f2065284a3b8c4b9651082
Arthur Flôr <arthurflor23@gmail.com>
2018-07-11 10:33:53 -0300

    Contact form

--- reactNativeCLI/app/views/Contact.js
+++ reactNativeCLI/app/views/Contact.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, TouchableHighlight } from 'react-native';
+import { Header } from '../sections/Header';
 
 export class Contact extends React.Component {
+
+  static navigationOptions = { header: null };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Enter Message',
+      name: 'Enter Name',
+      email: 'Enter Email Address',
+    };
+  }
+
+  clearFields = () => this.setState({msg:'', name:'', email:''});
+
+  sendMessage = () => {
+    Alert.alert(this.state.name, this.state.msg);
+    this.props.navigation.goBack();
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header message='Press to Login' />
+        <Text style={styles.heading}>Contact Us</Text>
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => this.setState({name: text})}
+          value={this.state.name}
+        />
+
+        <TextInput
+          style={styles.multiInput}
+          onChangeText={(text) => this.setState({msg: text})}
+          value={this.state.msg}
+          multiline={true}
+          numberOfLines={4}
+        />
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => this.setState({email: text})}
+          value={this.state.email}
+        />
+
+        <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
+          <Text style={styles.buttons} value={this.state.email}>Reset Form</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -21,5 +63,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    flex: 1,
+    fontSize: 16,
+  },
+  inputs: {
+    flex: 1,
+    width: '80%',
+    padding: 10,
+  },
+  multiInput: {
+    flex: 2,
+    width: '90%',
+    paddingTop: 20,
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16,
   },
 });
